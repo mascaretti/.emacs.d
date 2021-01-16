76af0b1d762719a6b6accd17d340adbc8316e98c
mascaretti <mascaretti@posteo.net>
2021-01-16 11:34:05 +0100

    Update init.el

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+;; Straight
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
@@ -10,6 +11,49 @@
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
+
+;; Org
+
+(add-hook 'org-mode-hook 'turn-on-flyspell)
+(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
+(setq org-log-done 'note)
+(setq org-agenda-files '("~/Dropbox/org"))
+(setq org-tag-alist '(("@writing" . ?w) ("@research" . ?r) ("personal" . ?p)))
+
+;; Org-Journal
+(straight-use-package 'org-journal)
+(setq org-journal-dir "~/Dropbox/org/journal/")
+(setq org-journal-date-format "%A, %d %B %Y")
+(require 'org-journal)
+(add-hook 'org-journal-hook 'turn-on-flyspell)
+
+;; Org-ref
+(straight-use-package 'org-ref)
+(straight-use-package 'helm-bibtex)
+(require 'org-ref)
+(setq reftex-default-bibliography '("~/Dropbox/bibliography/references.bib"))
+
+;; see org-ref for use of these variables
+(setq org-ref-bibliography-notes "~/Dropbox/bibliography/notes.org"
+      org-ref-default-bibliography '("~/Dropbox/bibliography/references.bib")
+      org-ref-pdf-directory "~/Dropbox/bibliography/bibtex-pdfs/")
+(setq bibtex-completion-bibliography "~/Dropbox/bibliography/references.bib"
+      bibtex-completion-library-path "~/Dropbox/bibliography/bibtex-pdfs"
+      bibtex-completion-notes-path "~/Dropbox/bibliography/helm-bibtex-notes")
+
+;; open pdf with system pdf viewer (works on mac)
+(setq bibtex-completion-pdf-open-function
+  (lambda (fpath)
+    (start-process "open" "*open*" "open" fpath)))
+
+;; alternative
+;; (setq bibtex-completion-pdf-open-function 'org-open-file)
+
+
+;; Writerroom Mode
+(straight-use-package 'writeroom-mode)
 
 ;; AUCTeX
 (straight-use-package 'auctex)
