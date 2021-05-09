37c527c733d75ab58fa9bb0520ffe4050678828b
Andrea Mascaretti <mascaretti@posteo.net>
2021-05-09 19:05:22 +0200

    Change init

--- init.el
+++ init.el
@@ -53,7 +53,6 @@
 
 ;; Org-ref
 (straight-use-package 'org-ref)
-(straight-use-package 'ivy-bibtex)
 (require 'org-ref)
 (setq reftex-default-bibliography '("~/gdrive/phd/references/zotLib.bib"))
 
@@ -81,18 +80,27 @@
 (setq org-ref-open-pdf-function 'my/org-ref-open-pdf-at-point)
 
 ;; open pdf with system pdf viewer (works on mac)
-(setq bibtex-completion-pdf-open-function
-  (lambda (fpath)
-    (start-process "open" "*open*" "open" fpath)))
+;; (setq bibtex-completion-pdf-open-function
+;;   (lambda (fpath)
+;;    (start-process "open" "*open*" "open" fpath)))
 
 ;; alternative
-;; (setq bibtex-completion-pdf-open-function 'org-open-file)
+(setq bibtex-completion-pdf-open-function 'org-open-file)
 
+;; Org-babel
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((R . t)))
+
+;; Helm-Bibtex
+(straight-use-package 'helm-bibtex)
+(setq bibtex-completion-notes-path "~/gdrive/phd/pdf_notes/notes.org")
 
 ;; Org-Roam
 (straight-use-package 'org-roam)
 (setq org-roam-directory "~/Dropbox/org-roam")
 (add-hook 'after-init-hook 'org-roam-mode)
+(setq org-roam-index-file "~/Dropbox/org-roam.index.org")
 
 (straight-use-package 'org-roam-bibtex)
 (use-package org-roam-bibtex
@@ -100,6 +108,51 @@
   :hook (org-roam-mode . org-roam-bibtex-mode)
   :config
   (require 'org-ref)) ; optional: if Org Ref is not loaded anywhere else, load it here
+
+(setq orb-templates
+      '(("r" "ref" plain (function org-roam-capture--get-point) ""
+         :file-name "${citekey}"
+         :head "#+TITLE: ${citekey}: ${title}\n#+ROAM_KEY: ${ref}\n" ; <--
+         :unnarrowed t)))
+(setq org-roam-capture-templates
+      '(("d" "default" plain (function org-roam--capture-get-point)
+     "%?"
+     :file-name "%<%Y%m%d%H%M%S>-${slug}"
+     :head "#+title: ${title}\n"
+     :unnarrowed t)))
+
+(setq orb-note-actions-interface 'hydra)
+
+
+;; Deft
+(straight-use-package 'deft)
+(use-package deft
+  :after org
+  :bind
+  ("C-c n d" . deft)
+  :custom
+  (deft-recursive t)
+  (deft-use-filter-string-for-filename t)
+  (deft-default-extension "org")
+  (deft-directory "~/Dropbox/org-roam/"))
+
+
+;; Org-downaload
+(straight-use-package 'org-download)
+(use-package org-download
+  :after org
+  :bind
+  (:map org-mode-map
+        (("s-Y" . org-download-screenshot)
+         ("s-y" . org-download-yank))))
+
+;; Mathpix
+(use-package mathpix.el
+  :straight (:host github :repo "jethrokuan/mathpix.el")
+  :custom ((mathpix-app-id "app-id")
+           (mathpix-app-key "app-key"))
+  :bind
+  ("C-x m" . mathpix-screenshot))
 
 ;; AUCTeX
 (straight-use-package 'auctex)
@@ -116,6 +169,9 @@
 (add-hook 'TeX-after-compilation-finished-functions
            #'TeX-revert-document-buffer)
 
+(straight-use-package 'cdlatex)
+(add-hook 'LaTeX-mode-hook 'turn-on-cdlatex) ; with Auctex
+
 
 ;; RefTeX
 (require 'reftex)
@@ -130,46 +186,11 @@
 (add-hook 'TeX-after-compilation-finished-functions #'TeX-revert-document-buffer)
 (straight-use-package 'org-noter)
 (pdf-loader-install)
+
+
 ;; ESS
 (straight-use-package 'ess)
 
-;; try mu4e
-(require 'mu4e)
-;; use mu4e for e-mail in emacs
-(setq mail-user-agent 'mu4e-user-agent)
-
-;; these are actually the defaults
-(setq
-  mu4e-sent-folder   "/Inviata"       ;; folder for sent messages
-  mu4e-drafts-folder "/Bozze"     ;; unfinished messages
-  mu4e-trash-folder  "/Cestino"      ;; trashed messages
-  mu4e-refile-folder "/archive")   ;; saved messages
-
-(setq mu4e-get-mail-command "mbsync -a")
-;; tell message-mode how to send mail
-
-(setq mu4e-compose-reply-to-address "mascaretti@stat.unipd.it"
-      user-email-address "mascaretti@stat.unipd.it"
-      user-full-name "Andrea Mascaretti")
-
-(setq mu4e-compose-signature
-      "Andrea Mascaretti")
-
-;; SMTP MAIL SETTING
-(setq
-   message-send-mail-function 'smtpmail-send-it
-   smtpmail-default-smtp-server "smtp.stat.unipd.it"
-   smtpmail-smtp-server "smtp.stat.unipd.it"
-   smtpmail-smtp-service 465
-   smtpmail-local-domain "unipd.it")
-
-   ;; if you need offline mode, set these -- and create the queue dir
-   ;; with 'mu mkdir', i.e.. mu mkdir /home/user/Maildir/queue
-   ;; smtpmail-queue-mail  nil
-   ;; smtpmail-queue-dir  "/home/user/Maildir/queue/cur")
-
-;; don't keep message buffers around
-(setq message-kill-buffer-on-exit t)
 
 ;; PROJECTILE
 (straight-use-package 'projectile)
@@ -178,57 +199,96 @@
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 (projectile-mode +1)
 
-;; IVY
-(straight-use-package 'counsel)
-(ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
-(setq ivy-count-format "(%d/%d) ")
+;; HELM
+(straight-use-package 'helm)
+(global-set-key (kbd "M-x") #'helm-M-x)
+(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
+(global-set-key (kbd "C-x C-f") #'helm-find-files)
+(helm-mode 1)
 
-(global-set-key (kbd "C-s") 'swiper-isearch)
-(global-set-key (kbd "M-x") 'counsel-M-x)
-(global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key (kbd "M-y") 'counsel-yank-pop)
-(global-set-key (kbd "<f1> f") 'counsel-describe-function)
-(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-(global-set-key (kbd "<f1> l") 'counsel-find-library)
-(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-(global-set-key (kbd "<f2> j") 'counsel-set-variable)
-(global-set-key (kbd "C-x b") 'ivy-switch-buffer)
-(global-set-key (kbd "C-c v") 'ivy-push-view)
-(global-set-key (kbd "C-c V") 'ivy-pop-view)
-
-(global-set-key (kbd "C-c c") 'counsel-compile)
-(global-set-key (kbd "C-c g") 'counsel-git)
-(global-set-key (kbd "C-c j") 'counsel-git-grep)
-(global-set-key (kbd "C-c L") 'counsel-git-log)
-(global-set-key (kbd "C-c k") 'counsel-rg)
-(global-set-key (kbd "C-c m") 'counsel-linux-app)
-(global-set-key (kbd "C-c n") 'counsel-fzf)
-(global-set-key (kbd "C-x l") 'counsel-locate)
-(global-set-key (kbd "C-c J") 'counsel-file-jump)
-(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
-(global-set-key (kbd "C-c w") 'counsel-wmctrl)
-
-(global-set-key (kbd "C-c C-r") 'ivy-resume)
-(global-set-key (kbd "C-c b") 'counsel-bookmark)
-(global-set-key (kbd "C-c d") 'counsel-descbinds)
-(global-set-key (kbd "C-c g") 'counsel-git)
-(global-set-key (kbd "C-c o") 'counsel-outline)
-(global-set-key (kbd "C-c t") 'counsel-load-theme)
-(global-set-key (kbd "C-c F") 'counsel-org-file)
+;; Company
+(straight-use-package 'company)
+(add-hook 'after-init-hook 'global-company-mode)
 
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-files
-   '("~/Dropbox/org/projects.org" "/home/andrea/Dropbox/org/Dreams.org" "/home/andrea/Dropbox/org/README.org" "/home/andrea/Dropbox/org/blog.org" "/home/andrea/Dropbox/org/francais.org" "/home/andrea/Dropbox/org/gtd.org" "/home/andrea/Dropbox/org/inbox.org" "/home/andrea/Dropbox/org/learning.org" "/home/andrea/Dropbox/org/someday.org" "/home/andrea/Dropbox/org/tickler.org" "/home/andrea/Dropbox/org/writing.org")))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; YASnippet
+(straight-use-package 'yasnippet)
+(straight-use-package 'yasnippet-snippets)
+(yas-global-mode 1)
+
+;; Stan
+
+(straight-use-package 'stan-mode)
+(straight-use-package 'company-stan)
+(straight-use-package 'eldoc-stan)
+(straight-use-package 'flycheck-stan)
+(straight-use-package 'stan-snippets)
+
+;; Uncomment the line below if not required elsewhere.
+;; (require 'use-package)
+
+;;; stan-mode.el
+(use-package stan-mode
+  :mode ("\\.stan\\'" . stan-mode)
+  :hook (stan-mode . stan-mode-setup)
+  ;;
+  :config
+  ;; The officially recommended offset is 2.
+  (setq stan-indentation-offset 2))
+
+;;; company-stan.el
+(use-package company-stan
+  :hook (stan-mode . company-stan-setup)
+  ;;
+  :config
+  ;; Whether to use fuzzy matching in `company-stan'
+  (setq company-stan-fuzzy nil))
+
+;;; eldoc-stan.el
+(use-package eldoc-stan
+  :hook (stan-mode . eldoc-stan-setup)
+  ;;
+  :config
+  ;; No configuration options as of now.
+  )
+
+;;; flycheck-stan.el
+(use-package flycheck-stan
+  ;; Add a hook to setup `flycheck-stan' upon `stan-mode' entry
+  :hook ((stan-mode . flycheck-stan-stanc2-setup)
+         (stan-mode . flycheck-stan-stanc3-setup))
+  :config
+  ;; A string containing the name or the path of the stanc2 executable
+  ;; If nil, defaults to `stanc2'
+  (setq flycheck-stanc-executable nil)
+  ;; A string containing the name or the path of the stanc2 executable
+  ;; If nil, defaults to `stanc3'
+  (setq flycheck-stanc3-executable nil))
+
+;;; stan-snippets.el
+(use-package stan-snippets
+  :hook (stan-mode . stan-snippets-initialize)
+  ;;
+  :config
+  ;; No configuration options as of now.
+  )
+
+;;; ac-stan.el (Not on MELPA; Need manual installation)
+(use-package ac-stan
+  :load-path "path-to-your-directory/ac-stan/"
+  ;; Delete the line below if using.
+  :disabled t
+  :hook (stan-mode . stan-ac-mode-setup)
+  ;;
+  :config
+  ;; No configuration options as of now.
+  )
+
+;; PYTHON
+(straight-use-package 'anaconda-mode)
+(straight-use-package 'elpy)
+(use-package elpy
+  :ensure t
+  :init
+  (elpy-enable))
+
